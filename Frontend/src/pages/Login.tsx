5ba81f6ad1722ec03978dd5ac37a6946633ed12c
Thomas Iafrate <113009615+thomasiafrate1@users.noreply.github.com>
2025-06-27 12:40:04 +0200

    Commit file /Frontend/src/pages/Login.tsx
    Create Login.tsx

---
+++ Frontend/src/pages/Login.tsx
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../styles/login.css"
+
+export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+
+    try {
+      const res = await fetch("http://192.168.1.42:3000/api/admin/login", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await res.json();
+
+      if (!res.ok) {
+        setError(data.message || "Erreur lors de la connexion");
+        return;
+      }
+
+      localStorage.setItem("token", data.token);
+      navigate("/dashboard"); // Redirige vers le tableau de bord admin
+    } catch (err) {
+      setError("Erreur serveur");
+    }
+  };
+
+  return (
+    <div className="login-container">
+      <h1 style={{color:"black"}}>Connexion Admin</h1>
+      <form onSubmit={handleLogin} className="login-form">
+        <label>Email :</label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+
+        <label>Mot de passe :</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+
+        <button type="submit">Se connecter</button>
+
+        {error && <p className="error">{error}</p>}
+      </form>
+    </div>
+  );
+}
