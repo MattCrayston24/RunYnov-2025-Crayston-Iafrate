6db8792ad42d48ee1f6a64072b9c4846ddcc2152
MattCrayston24 <113009631+MattCrayston24@users.noreply.github.com>
2025-06-27 12:23:03 +0200

    Commit folder /App/services
    Replacement files authService.ts and courseService.ts with the olds ones

---
+++ App/services/authService.ts
@@ -0,0 +1,26 @@
+const API_URL = "http://192.168.1.42:3000/api";
+
+export const login = async (email: string, password: string) => {
+  const res = await fetch(`${API_URL}/auth/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
+  return await res.json();
+};
+
+export const register = async (email: string, password: string, username: string) => {
+  const res = await fetch(`${API_URL}/auth/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password, username }),
+  });
+
+  const data = await res.json();
+
+  if (!res.ok) {
+    console.log("Erreur HTTP:", res.status, data);
+  }
+
+  return data;
+};
